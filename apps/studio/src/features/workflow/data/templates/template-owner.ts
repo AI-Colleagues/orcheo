68afb7d3fbc85cd83f40ff073b79d72dbde4859f
Shaojie Jiang <shaojie.jiang1@gmail.com>
2026-06-25 13:53:06 +0200

    Clean up unused dependencies and workflow helpers (#420)

--- apps/studio/src/features/workflow/data/templates/template-owner.ts
+++
@@ -1,7 +0,0 @@
-import { Workflow } from "../workflow-types";
-
-export const TEMPLATE_OWNER: Workflow["owner"] = {
-  id: "team-templates",
-  name: "Orcheo Templates",
-  avatar: "https://avatar.vercel.sh/templates",
-};
